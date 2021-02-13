f109fe935115f66886b867043056ddbd7ab67185
paigevenuto <paigevenuto@gmail.com>
2021-02-13 05:09:04 -0500

    ready to begin testing

--- public/copy_link.js
+++ public/copy_link.js
@@ -4,5 +4,5 @@ linkBtn.addEventListener("click", () => {
   linkBtn.select();
   linkBtn.setSelectionRange(0, 99999);
   document.execCommand("copy");
-  document.getElementById("clip-icon").style.background("#bf3858");
+  document.getElementById("clip-icon").style.background = "#bf3858";
 });
