6856b11258ef25d589658ed5516e5e913b87ea4e
paigevenuto <paigevenuto@gmail.com>
2021-02-13 05:41:14 -0500

    fixing TESTING_MODE config var

--- db.js
+++ db.js
@@ -3,14 +3,15 @@ const { DATABASE_URL, TESTING_MODE } = require("./config");
 const pg = require("pg");
 // TO fix H12 errors: https://devcenter.heroku.com/articles/heroku-postgresql#heroku-postgres-ssl
 //
-const dbConfig = TESTING_MODE
-  ? {
-      connectionString: DATABASE_URL,
-    }
-  : {
-      connectionString: DATABASE_URL,
-      ssl: { rejectUnauthorized: false },
-    };
+const dbConfig =
+  TESTING_MODE === "true"
+    ? {
+        connectionString: DATABASE_URL,
+      }
+    : {
+        connectionString: DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      };
 
 const db = new pg.Client(dbConfig);
 db.connect();
