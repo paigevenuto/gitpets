2dc53b1ed3f969f1aa261f43f53ace2b89f6facd
paigevenuto <paigevenuto@gmail.com>
2021-02-13 05:38:33 -0500

    fixed copy to clipboard button

--- db.js
+++ db.js
@@ -1,12 +1,17 @@
-const { DATABASE_URL } = require("./config");
+const { DATABASE_URL, TESTING_MODE } = require("./config");
 
 const pg = require("pg");
 // TO fix H12 errors: https://devcenter.heroku.com/articles/heroku-postgresql#heroku-postgres-ssl
-const db = new pg.Client({
-  connectionString: DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+//
+const dbConfig = TESTING_MODE
+  ? {
+      connectionString: DATABASE_URL,
+    }
+  : {
+      connectionString: DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    };
+
+const db = new pg.Client(dbConfig);
 db.connect();
 module.exports = db;
--- public/copy_link.js
+++ public/copy_link.js
@@ -1,8 +1,13 @@
 const linkBtn = document.getElementById("copy-link");
+const username = linkBtn.value;
+const shareUrl = `<a href="https://gitpets.herokuapp.com/user/${username}"><img src="https://gitpets.herokuapp.com/pet/${username}"></a>`;
+linkBtn.value = shareUrl;
 
-linkBtn.addEventListener("click", () => {
+const copyToClipboard = () => {
   linkBtn.select();
   linkBtn.setSelectionRange(0, 99999);
   document.execCommand("copy");
   document.getElementById("clip-icon").style.background = "#bf3858";
-});
+};
+
+document.getElementById("clip-icon").addEventListener("click", copyToClipboard);
